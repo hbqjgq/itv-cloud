2e9f624c2b15ba9fad05398fbf291f963641a9b2
hbqjgq <458909264@qq.com>
2018-09-21 17:41:59 +0800

    itv-cloud 初版

---
+++ itv-core-server/src/main/java/com/kwok/itvcoreserver/ItvCoreServerApplication.java
@@ -0,0 +1,12 @@
+package com.kwok.itvcoreserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItvCoreServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItvCoreServerApplication.class, args);
+	}
+}
